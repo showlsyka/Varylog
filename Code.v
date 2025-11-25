9f0294fe543cd2e09c2108d95e6e9834cea7102d
Showls <151879879+showlsyka@users.noreply.github.com>
2025-11-25 19:47:51 +0300

    Update Code.v

--- Code.v
+++ Code.v
@@ -39,9 +39,41 @@ nor(n11,x[2],x[2]); nor(n12,n11,x[0]); nor(y[4],n12,n12);//y4 n11 nx2x2
 
 nor(n13,n11,n3); nor(y[5],n13,n13);//y5
 end
-endgenerate
 
-else if(Realization == "SHEFFER")
+else if (Realization == "SHEFER")
+begin
+wire [2:0] nx; 
+wire[1:0] ny;
+nand (nx[2], x[2], x[2]); // ~x2
+nand (nx[1], x[1], x[1]); // ~x1
+nand (nx[0], x[0], x[0]); // ~x0
+
+nand (ny[0],nx[2],nx[1]); // ~x1 * ~x2 - > x1 + x2
+nand (ny[1],ny[0],ny[0]); // x1 + x2   - > ~x1 * ~x2
+
+nand (y[0],ny[1],nx[0]);  // y0  
+nand (y[1],ny[1],x[0]);   // y1
+nand(y[2],nx[0], x[1]);   // y2
+nand(y[3],x[0], x[1]);    // y3
+nand(y[4],nx[0], x[2]);   //y4
+nand(y[5],x[2], x[0]);    //y5
+end
+
+else if  (Realization == "ZHEG")
+begin
+and(x1x0,x[1],x[0]);and(x2x0,x[2],x[0]);and(x2x1,x[2],x[1]);and(x2x1x0,x2x1,x[0]);
+xor(z1,x[0],x[1]);xor(z2,x1x0,z1);xor(z3,x[2],z2);
+xor(z4,x2x0,z3);xor(z5,x2x1,z4);xor(y[0],x2x1x0,z5);
+
+xor(z6, x[0], x1x0);xor(z7,z6,1);xor(y[1],z7,x2x0);
+
+xor(z8,1,x[1]);xor(y[2],z8,x1x0);
+xor(y[3],1,x1x0);
+xor(z9,1,x[2]);xor(y[4],z9,x2x0);
+xor(y[5],1,x2x0);
+end
+
+endgenerate
 
 endmodule
 
