6a0c2cced4c218cfe48bbc1fd311ddb67fb8b1cb
Showls <151879879+showlsyka@users.noreply.github.com>
2026-03-10 20:27:28 +0300

    Update lr5zd5.v

--- lr5zd5.v
+++ lr5zd5.v
@@ -11,33 +11,62 @@ endmodule
 
 module lr5zd5 #(parameter mode="SUM") (output [3:0] b, input clk);
 
-    wire [3:0] next;
+    reg  [3:0] next;     // next ???????? ? always @*
+    wire [3:0] j;
+    wire [3:0] k;
 
-    assign next =
-        (b==4'b0000) ? 4'b0001 :
-        (b==4'b0001) ? 4'b0011 :
-        (b==4'b0011) ? 4'b0010 :
-        (b==4'b0010) ? 4'b0110 :
-        (b==4'b0110) ? 4'b0111 :
-        (b==4'b0111) ? 4'b0101 :
-        (b==4'b0101) ? 4'b0100 :
-        (b==4'b0100) ? 4'b1100 :
-        (b==4'b1100) ? 4'b1101 :
-        (b==4'b1101) ? 4'b1111 :
-        (b==4'b1111) ? 4'b1110 :
-        (b==4'b1110) ? 4'b1010 :
-        (b==4'b1010) ? 4'b1011 :
-        (b==4'b1011) ? 4'b1001 :
-        (b==4'b1001) ? 4'b1000 :
-        (b==4'b1000) ? 4'b0000 :
-                      4'b0000;  // ?? ?????? ??????
+    always @* begin
+        // default (????? ?? ???? latch)
+        next = 4'b0000;
 
-    // 0->1: J=1 K=0
-    // 1->0: J=0 K=1
-    // 0->0: J=0 K=0
-    // 1->1: J=0 K=0
-    wire [3:0] j = (~b) & next;
-    wire [3:0] k = ( b) & (~next);
+        if (mode == "SUM") begin
+            // -------- SUM (Gray UP) --------
+            if      (b==4'b0000) next = 4'b0001;
+            else if (b==4'b0001) next = 4'b0011;
+            else if (b==4'b0011) next = 4'b0010;
+            else if (b==4'b0010) next = 4'b0110;
+            else if (b==4'b0110) next = 4'b0111;
+            else if (b==4'b0111) next = 4'b0101;
+            else if (b==4'b0101) next = 4'b0100;
+            else if (b==4'b0100) next = 4'b1100;
+            else if (b==4'b1100) next = 4'b1101;
+            else if (b==4'b1101) next = 4'b1111;
+            else if (b==4'b1111) next = 4'b1110;
+            else if (b==4'b1110) next = 4'b1010;
+            else if (b==4'b1010) next = 4'b1011;
+            else if (b==4'b1011) next = 4'b1001;
+            else if (b==4'b1001) next = 4'b1000;
+            else if (b==4'b1000) next = 4'b0000;
+            else                 next = 4'b0000;
+        end
+        else if (mode == "SUB") begin
+            // -------- SUB (Gray DOWN) --------
+            if      (b==4'b0000) next = 4'b1000;
+            else if (b==4'b0001) next = 4'b0000;
+            else if (b==4'b0011) next = 4'b0001;
+            else if (b==4'b0010) next = 4'b0011;
+            else if (b==4'b0110) next = 4'b0010;
+            else if (b==4'b0111) next = 4'b0110;
+            else if (b==4'b0101) next = 4'b0111;
+            else if (b==4'b0100) next = 4'b0101;
+            else if (b==4'b1100) next = 4'b0100;
+            else if (b==4'b1101) next = 4'b1100;
+            else if (b==4'b1111) next = 4'b1101;
+            else if (b==4'b1110) next = 4'b1111;
+            else if (b==4'b1010) next = 4'b1110;
+            else if (b==4'b1011) next = 4'b1010;
+            else if (b==4'b1001) next = 4'b1011;
+            else if (b==4'b1000) next = 4'b1001;
+            else                 next = 4'b0000;
+        end
+        else begin
+            next = 4'b0000;  
+        end
+    end
+
+    // ??????????? JK (0/1, ??? ?)
+    assign j = (~b) & next;
+    assign k = ( b) & (~next);
 
     genvar i;
     generate
@@ -53,12 +82,14 @@ endmodule
 `define TD 10
 `include "lr5zd5.vhd"
 
-
 module lr5zd5_tb();
     reg clk=0;
     wire [3:0] b;
-    
-    lr5zd5 #(.mode("SUM")) counter (.b(b), .clk(clk));
-    initial
+
+    lr5zd5 #(.mode("SUB")) counter (.b(b), .clk(clk));
+
+    initial begin
         repeat (64) #`TD clk=~clk;
+        $finish;
+    end
 endmodule
